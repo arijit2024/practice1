20e5959f42d8a28f070a60eb9d974bb2e5d2f909
arijit <arijitsarkar2111@gmail.com>
2024-01-10 00:27:38 +0530

    update

--- server/router/auth.js
+++ server/router/auth.js
@@ -37,7 +37,7 @@ router.post("/register", (req,res) => {
     const  user = new User({name,email,phone,work,password,cpassword});
     user.save().then(()=>{
         res.status(201).json({message:"user register successfuly"});
-    }).catch((err)=>{req.status(500).json({message:"fails to register"})})
+    }).catch((err)=>{req.status(500).json({message:"fails to Register"})})
 
 });
 
